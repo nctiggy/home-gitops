810d8a7d5d89b39ebe8025b0ebac217178b3edef
Craig Smith <nctiggy@gmail.com>
2020-08-21 14:02:41 -0700

    update vault ingress

--- setup/bootstrap-cluster.sh
+++ setup/bootstrap-cluster.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ flux --values "$REPO_ROOT"/flux/flux/flux-values.yaml --namespace flux fluxcd/flux#!/bin/bash
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
@@ -21,7 +21,7 @@ installFlux() {
   kubectl create namespace flux
   kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/crds.yaml
   helm repo add fluxcd https://charts.fluxcd.io
-  helm upgrade --install flux --values "$REPO_ROOT"/flux/flux/flux-values.yaml --namespace flux fluxcd/flux
+  helm upgrade --install flux --values "$REPO_ROOT"/flux/flux/flux-helm-values.yaml --namespace flux fluxcd/flux
   helm upgrade --install helm-operator --values "$REPO_ROOT"/flux/helm-operator/flux-helm-operator-values.yaml --namespace flux fluxcd/helm-operator
 
   FLUX_READY=1
@@ -40,8 +40,8 @@ installFlux() {
 }
 
 installFlux
-"$REPO_ROOT"/setup/bootstrap-objects.sh
-"$REPO_ROOT"/setup/bootstrap-vault.sh
+#"$REPO_ROOT"/setup/bootstrap-objects.sh
+#"$REPO_ROOT"/setup/bootstrap-vault.sh
 
 message "all done!"
 kubectl get nodes -o=wide
